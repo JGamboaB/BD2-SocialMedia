be9915480ff98761ff2a193758f589ba9b59b898
JGamboaB <jgamboabotero@gmail.com>
2022-03-28 22:47:42 -0600

    Inicio

---
+++ socialmedia/src/components/topbar/Topbar.jsx
@@ -0,0 +1,13 @@
+import "./topbar.css"
+
+export default function topbar() {
+  return (
+    <div className="topbarContainer">
+        <div className="topbarLeft">
+          <span className="logo"></span>
+        </div>
+        <div className="topbarCenter"></div>
+        <div className="topbarRight"></div>
+    </div>
+  )
+}

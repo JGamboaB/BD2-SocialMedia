0a6d6be829aee2d74eba5397c0e9ad3f142b84c6
JGamboaB <jgamboabotero@gmail.com>
2022-04-01 23:36:00 -0600

    Profile

--- socialmedia/src/components/rightbar/Rightbar.jsx
+++ socialmedia/src/components/rightbar/Rightbar.jsx
@@ -1,11 +1,12 @@
 import './rightbar.css'
 
 // For friends
-export default function Rightbar() {
-  return (
-    <div className='rightbar'>
-      <div className="rightbarWrapper">
-        <h4 className='rightbarTitle'>Friends</h4>
+export default function Rightbar({profile}) {
+
+  const HomeRightbar = () => {
+    return(
+      <>
+      <h4 className='rightbarTitle'>Friends</h4>
         <ul className="rightbarFriendList">
 
           <li className="rightbarFriend">
@@ -35,6 +36,34 @@ export default function Rightbar() {
           </li>
 
         </ul>
+      </>
+    )
+  }
+
+  const ProfileRightbar = () => {
+    return(
+      <div className='rightbarProfile'>
+
+      <h4 className="rightbarTitle"> User info</h4>
+      <div className="rightbarInfo">
+        <div className="rightbarInfoItem">
+          <span className="rightbarInfoKey">Intereses:</span>
+          <span className="rightbarInfoValue">FNAF</span>
+        </div>
+        <div className="rightbarInfoItem">
+          <span className="rightbarInfoKey">Hobbies:</span>
+          <span className="rightbarInfoValue">Working</span>
+        </div>
+      </div>
+
+      </div>
+    )
+  }
+
+  return (
+    <div className='rightbar'>
+      <div className="rightbarWrapper">
+      {profile ? <ProfileRightbar /> : <HomeRightbar />}
       </div>
     </div>
   )
